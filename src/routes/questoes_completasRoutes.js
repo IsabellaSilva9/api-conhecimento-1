d2737f6f48df7ebad74f77090fedfa5bf03656a5
IsabellaSilva9 <isabella09sdj@gmail.com>
2026-04-07 15:46:22 -0300

    .

--- src/routes/questoes_completasRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const questoes_completasControllers = require("../controllers/questoes_completasControllers");
-
-// ÚNICA rota GET
-router.get("/completo", questoes_completasControllers.listar);
-
-module.exports = router;
